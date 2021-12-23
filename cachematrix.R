c1ba4bb496871e129fb4caa3346f3f0835674134
Costa Luis F <CostaLuisF@JohnDeere.com>
2021-12-23 11:25:42 -0300

    remove test hub lines comments solve the exercise

--- cachematrix.R
+++ cachematrix.R
@@ -4,23 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-  ## teste para ver se clonou
-  ## teste 1 
-  ## teste 2
-  
-  ## teste 3
-  
-  ## teste 4
-  
-  
-  
-  
+  i <- NULL ##Set the i to a NULL for future placeholder  
+  set <- function(y) { ##function to set the vector X to new vector Y and reset the the i to NULL
+    x <<- y
+    i <<- NULL 
+  }
+  get <- function() x #retirn the vector x
+  setinverse <- function(inverse) i <<- inverse ##set the i to inverse
+  getinverse <- function() i ##return the inverse i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) ##return the vector containing all functions defined
 }
-
-
+  
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if (!is.null(i)) {
+      message("getting cached data")
+      return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
+  
+ ## Return a matrix that is the inverse of 'x'
+
